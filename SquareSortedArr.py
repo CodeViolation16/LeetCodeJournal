0baa9f21dae8090a4a2c52c2b7f3abd956d7e792
Khanh Nguyen <107160291+CodeViolation16@users.noreply.github.com>
2024-08-05 22:40:18 -0500

    Update SquareSortedArr.py

--- SquareSortedArr.py
+++ SquareSortedArr.py
@@ -1,1 +1,26 @@
 # stack problem
+# my version 
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        newArray = [i**2 for i in nums]
+        return sorted(newArray)
+
+# chad version 
+
+
+ def sortedSquares(self, nums: List[int]) -> List[int]:
+        left = 0
+        right = len(nums) - 1 
+        result = []
+
+        while left <= right: 
+            if abs(nums[left]) > abs(nums[right]):
+                result.append(nums[left] ** 2)
+                left += 1
+            else: 
+                result.append(nums[right] ** 2)
+                right -= 1
+                
+        result.reverse()
+        return result
